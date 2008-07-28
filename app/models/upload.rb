228eee007b4e3f0652b138dee9201bd2f86b1882
Mutwin Kraus <mutle@blogage.de>
2008-07-28 18:02:51 +0200

    fix for small uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -9,7 +9,11 @@ class Upload < ActiveRecord::Base
     self.file_name = uploaded_file.original_filename.gsub(/\.([^\.]*)$/, '')
     self.file_ext = File.extname(uploaded_file.original_filename).gsub(/^\./, '')
     self.file_id = Digest::SHA1.hexdigest("#{Time.now.to_i}_#{self.user_id}_#{self.file_name}")
-    FileUtils.move uploaded_file.local_path, file_path
+    if uploaded_file.instance_of?(Tempfile)
+      FileUtils.move uploaded_file.local_path, file_path
+    else
+      File.open(file_path, "w") { |f| f.write(uploaded_file.read) }
+    end
   end
   
   def download_path
