cf03ecbb48b20d284abc8c22879c95b30e0f74b7
Mutwin Kraus <mutwin.kraus@gmail.com>
2012-01-28 22:08:00 +0100

    Fix channel list last poster

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   
   belongs_to :channel
   belongs_to :user
+
+  scope :first_channel_post, proc { |c| includes(:user).where(:channel_id => c.id).order("created_at DESC").limit(1) }
   
   after_create :delete_channel_visits
   after_create :update_channel_last_post
