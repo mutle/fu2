5800d235496dd2290ab8b59103faa805ac9720fb
Mutwin Kraus <mutwin.kraus@gmail.com>
2013-10-20 16:43:21 +0200

    Show stats on profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,10 @@ class UsersController < ApplicationController
       User.first(:conditions => ["LOWER(login) = LOWER(?)", params[:id]]) || raise(ActiveRecord::RecordNotFound)
     end
     @column_width = 12
+    @user_posts = @user.posts.all(:limit => 10, :order => "posts.created_at DESC", :include => :channel, :conditions => ["channels.default_read = ? AND channels.default_write = ?", true, true])
+    @user_faves = @user.faves.includes(:post => :channel).limit(10)
+    @user_faves_received = Fave.includes(:post).where("posts.user_id = ?", @user.id).count
+    @user_channel_count = Channel.where("user_id = ?", @user.id).count
     respond_with @user
   end
 
