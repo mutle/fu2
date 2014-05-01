ba7ef70b8d47d8006a2fe7d64b1d8399dbdda613
Mutwin Kraus <mutwin.kraus@gmail.com>
2014-05-01 21:25:37 +0200

    Add deployed command

--- lib/fubot/commands/fubot_deploy.rb
+++ lib/fubot/commands/fubot_deploy.rb
@@ -1,3 +1,14 @@
+Fubot.command /deployed\?/ do
+  def help
+    ["deployed", "Shows the currently deployed commit"]
+  end
+
+  def call(bot, args, message)
+    info = `git show --oneline -s`.split(" ", 2)
+    bot.reply "[#{info.first}](https://github.com/mutle/fu2/commit/#{info.first}) #{info.last}"
+  end
+end
+
 Fubot.command /deploy ?([^ ]*)/ do
   DEPLOY_USERS = ["mutle"]
   DEPLOY_SCRIPT = proc { |branch| "/bin/bash -c \". /data/fu2/shared/env && cd /data/fu2/current && git fetch origin && cp config/database.yml{,.bak} && git reset --hard origin/#{branch} && cp config/database.yml{.bak,} && bundle install && bundle exec rake assets:precompile && kill -HUP \`cat /data/fu2/shared/unicorn.pid\` && kill -HUP `cat /data/fu2/shared/resqued.pid`\"" }
