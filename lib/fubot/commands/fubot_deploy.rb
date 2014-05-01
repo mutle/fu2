145eaf597aced9bf108c2f1b179b8724153d082e
Mutwin Kraus <mutwin.kraus@gmail.com>
2014-05-01 21:56:19 +0200

    Format deploy output

--- lib/fubot/commands/fubot_deploy.rb
+++ lib/fubot/commands/fubot_deploy.rb
@@ -11,10 +11,10 @@ end
 
 Fubot.command /deploy ?([^ ]*)/ do
   DEPLOY_USERS = ["mutle"]
-  DEPLOY_SCRIPT = proc { |branch| "/bin/bash -c \". /data/fu2/shared/env && cd /data/fu2/current && git fetch origin && cp config/database.yml{,.bak} && git reset --hard origin/#{branch} && cp config/database.yml{.bak,} && bundle install && bundle exec rake assets:precompile && kill -HUP \`cat /data/fu2/shared/unicorn.pid\` && kill -HUP `cat /data/fu2/shared/resqued.pid`\"" }
+  DEPLOY_SCRIPT = proc { |branch| "/bin/bash -c \". /data/fu2/shared/env && cd /data/fu2/current && git fetch origin && cp config/database.yml{,.bak} && git reset --hard origin/#{branch} && cp config/database.yml{.bak,} && bundle install >/dev/null && bundle exec rake assets:precompile && kill -HUP \`cat /data/fu2/shared/unicorn.pid\` && kill -HUP `cat /data/fu2/shared/resqued.pid`\"" }
 
   def help
-    ["deploy", "Deploys the redcursor app"]
+    ["deploy [branch]", "Deploys the redcursor app"]
   end
 
   def call(bot, args, message)
@@ -25,7 +25,8 @@ Fubot.command /deploy ?([^ ]*)/ do
     else
       bot.reply "Deploying #{branch} to production."
       script = DEPLOY_SCRIPT.call(branch)
-      bot.reply `#{script}`
+      result = `#{script}`
+      bot.reply "```\n#{result}\n```"
       bot.reply "Deploy completed."
     end
   end
