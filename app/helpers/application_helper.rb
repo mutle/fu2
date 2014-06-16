adf5c3cc03ea0f3a30933d45917fa7dec44abc7f
Mutwin Kraus <mutwin.kraus@gmail.com>
2014-06-16 22:46:24 +0200

    Use octicons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     javascript_tag "window.Users = #{User.all.map { |u| {:login => u.login, :id => u.id, :avatar_url => u.avatar_image_url(22) } }.to_json};"
   end
 
+  def octicon(name, mega=false)
+    content_tag :span, "", :class => "#{'mega-' if mega}octicon octicon-#{name}"
+  end
+
 end
