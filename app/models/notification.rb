da00facd33861cb8fb4735970af7dbc30b97cb00
Mutwin Kraus <mutwin.kraus@gmail.com>
2014-09-24 16:18:17 +0200

    Fix mention invalidation

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -13,7 +13,7 @@ class Notification < ActiveRecord::Base
   scope :messages, proc { where(:notification_type => "message") }
   scope :mentions, proc { where(:notification_type => "mention") }
   scope :toolbar_notifications, proc { where(:notification_type => ["message", "mention"]) }
-  scope :from_user, proc { |user| messages.where(:created_by_id => user.id) }
+  scope :from_user, proc { |user| where(:created_by_id => user.id) }
 
   class << self
     def mention(from, to, channel, post)
