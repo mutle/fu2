35cbe43c09ac92bbc683f88f07ca4eb26ad2d3a9
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-06-26 23:50:21 +0200

    Cleanup unused CJSX stuff

--- config/initializers/cjsx.rb
+++
@@ -1,4 +0,0 @@
-require 'cjsx'
-
-Fu2::Application.assets.register_engine '.cjsx', React::CJSX::Template
-Fu2::Application.config.watchable_files.concat Dir["#{Fu2::Application.root}/app/assets/javascripts/**/*.cjsx*"]
