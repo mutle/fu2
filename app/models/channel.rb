af9d9809b64930f3689e4a973d8f23ec0018757b
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-07-04 19:45:28 +0200

    Fix channel list read display

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -230,9 +230,9 @@ class Channel < ActiveRecord::Base
       $redis.zadd "mentions:#{current_user.id}", 0, id
       Live.notification_counters(current_user) if num && num.to_i > 0
     end
-    post_id ||= (last_post.try(:id) || 0)
+    post_id ||= (last_post_id || 0)
     i = last_read_id(current_user).to_i
-    Live.posts_read(self, current_user) if i != post_id
+    Live.posts_read(self, current_user) if user && i != post_id
     $redis.zadd "last-post:#{current_user.id}", post_id, id
     Notification.for_user(current_user).mentions.in_channel(self).unread.update_all(:read => true)
     i
