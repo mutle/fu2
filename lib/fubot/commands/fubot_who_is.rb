6134a16e1ff07b552c7461862111f8438ba81e77
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-11-09 01:35:12 +0100

    Fix fubot who is command

--- lib/fubot/commands/fubot_who_is.rb
+++ lib/fubot/commands/fubot_who_is.rb
@@ -7,7 +7,8 @@ Fubot.command /who is @?([^ ]+)\??/i do
     user = args[1]
     values = KeyValue.get("Fubot:Whois:#{user}[]")
     if values.size > 0
-      bot.reply "#{user} is: #{values.join(", ")}"
+      t = values.map { |v| m = v.split(":", 2); "#{m[1]} -- #{User.find(m[0].to_i).login}" }.join(", ")
+      bot.reply "#{user} is: #{t}"
     else
       bot.reply "I don't know anything about #{user}."
     end
