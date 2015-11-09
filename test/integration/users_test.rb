8ada5cbf0dedfabe0efd2cc7c1e5c2334b9e7f80
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-11-09 01:21:45 +0100

    Remove unneeded stuff

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -6,18 +6,4 @@ class UsersTest < ActionDispatch::IntegrationTest
     login_user
   end
 
-  test "show other user" do
-    u = create_user
-    get "/users/#{u.id}"
-    assert_select ".user-name", u.login
-  end
-
-  test "show users json" do
-    u = create_user
-    get "/users.json"
-    ju = json_body['users'].last
-    assert_equal u.id, ju['id']
-    assert_equal u.login, ju['login']
-  end
-
 end
