7405f60ddce44ec66e4849b4e7179a5e0f96a107
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-11-27 21:12:32 +0100

    Invalidate cache if it got changed and some fixes for keyvalue

--- lib/fubot/commands/fubot_who_is.rb
+++ lib/fubot/commands/fubot_who_is.rb
@@ -5,7 +5,7 @@ Fubot.command /who is @?([^ ]+)\??/i do
 
   def call(bot, args, message)
     user = args[1]
-    values = KeyValue.get("Fubot:Whois:#{user}[]")
+    values = KeyValue.get(bot.site, "Fubot:Whois:#{user}[]")
     if values.size > 0
       t = values.map { |v| m = v.split(":", 2); "#{m[1]} -- #{User.find(m[0].to_i).login}" }.join(", ")
       bot.reply "#{user} is: #{t}"
@@ -23,7 +23,7 @@ Fubot.command /remember ([^ ]+) is (.+)/i do
   def call(bot, args, message)
     user = args[1]
     args[2].split(",").each do |title|
-      KeyValue.set("Fubot:Whois:#{user}[]", "#{bot.user.id}:#{title.chomp}")
+      KeyValue.set(bot.site, "Fubot:Whois:#{user}[]", "#{bot.user.id}:#{title.chomp}")
     end
     bot.reply "I'll remember this."
   end
