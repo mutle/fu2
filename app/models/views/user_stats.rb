3ca9e9d77ed3e9bc134bd491e5590b51a024ad37
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-11-27 04:12:43 +0100

    Scope for user whois

--- app/models/views/user_stats.rb
+++ app/models/views/user_stats.rb
@@ -9,7 +9,7 @@ module Views
     fetches :faves_received, proc { Fave.site_scope(site).includes(:post).where("posts.user_id = ?", @user.id).references(:post).count }
 
     fetches :emojis, proc { Fave.user_emojis(site, user) }
-    fetches :title, proc { KeyValue.get("Fubot:Whois:#{user.login}[]") }
+    fetches :title, proc { KeyValue.site_scope(site).get("Fubot:Whois:#{user.login}[]") }
 
     fetches :last_posts, proc { @user.posts.site_scope(site).where("channels.default_read = ? AND channels.default_write = ?", true, true).limit(5).order("posts.created_at DESC").includes(:channel).references(:channel) }
     fetches :last_faves, proc { @user.faves.site_scope(site).includes(:post).order("faves.created_at DESC").limit(5) }
