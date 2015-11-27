d96d0a4b791f56ecbda1e449ca339306899562d2
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-11-27 03:06:34 +0100

    Another attempt at fixing channel load bug

--- app/assets/javascripts/components/channel-posts.jsx
+++ app/assets/javascripts/components/channel-posts.jsx
@@ -231,7 +231,7 @@ var ChannelPosts = React.createClass({
             return <ChannelEvent key={"event-"+post.id} id={post.id} event={post} user={user} />;
           } else {
             pi++;
-            return <ChannelPost key={"post-"+post.id} id={post.id} highlight={pi - 1 == highlight} channelId={channelId} user={user} post={post} posts={self} editable={user.id == Data.user_id} bodyClick={self.bodyClick} />;
+            return <ChannelPost key={"post-"+post.id} id={post.id} highlight={pi - 1 == highlight} channelId={channelId} user={user} post={post} posts={self} editable={user &&& user.id == Data.user_id} bodyClick={self.bodyClick} />;
           }
         });
       }
