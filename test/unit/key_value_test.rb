fdd3ccd86af85c7140387f73550aa90024e20abb
Mutwin Kraus <mutwin.kraus@gmail.com>
2015-11-27 21:16:17 +0100

    Fix keyvalue test

--- test/unit/key_value_test.rb
+++ test/unit/key_value_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class KeyValueTest < ActiveSupport::TestCase
   test "set key value" do
     t = Time.now.to_f.to_s
-    assert_nil KeyValue.get("test-#{t}")
-    KeyValue.set("test-#{t}", "1")
-    assert_equal "1", KeyValue.get("test-#{t}")
+    assert_nil KeyValue.get(@site, "test-#{t}")
+    KeyValue.set(@site, "test-#{t}", "1")
+    assert_equal "1", KeyValue.get(@site, "test-#{t}")
   end
 
   test "add to array value" do
     t = Time.now.to_f.to_s
-    assert_equal [], KeyValue.get("test-#{t}[]")
-    KeyValue.set("test-#{t}[]", "1")
-    assert_equal ["1"], KeyValue.get("test-#{t}[]")
-    KeyValue.set("test-#{t}[]", "2")
-    assert_equal ["1", "2"], KeyValue.get("test-#{t}[]")
+    assert_equal [], KeyValue.get(@site, "test-#{t}[]")
+    KeyValue.set(@site, "test-#{t}[]", "1")
+    assert_equal ["1"], KeyValue.get(@site, "test-#{t}[]")
+    KeyValue.set(@site, "test-#{t}[]", "2")
+    assert_equal ["1", "2"], KeyValue.get(@site, "test-#{t}[]")
   end
 end
