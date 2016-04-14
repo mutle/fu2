171e8a94dc4a44918fa85076417b2d39bfb95a38
Mutwin Kraus <mutwin.kraus@gmail.com>
2016-04-14 20:18:39 +0200

    Precompile test.js for test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,8 @@ Fu2::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.assets.precompile += %w(test.js)
+
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
