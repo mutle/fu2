62df9a0da0003a7448eb986b7802f2d836053301
Mutwin Kraus <mutwin.kraus@gmail.com>
2016-04-14 20:47:31 +0200

    Fix view dependencies

--- app/models/views/channel_list.rb
+++ app/models/views/channel_list.rb
@@ -6,19 +6,15 @@ module Views
     fetches :filter_ids, proc { Channel.filter_ids(site, query, current_user) }
     fetches :recent_channels, proc { Channel.recent_channels(site, current_user, page, per_page, last_update_date, filter_ids) }, [:filter_ids]
     fetches :channels_read, proc {
-      recent_channels.each do |channel|
-        channel.read = channel.has_posts?(current_user, channel.last_post)
-      end
+      recent_channels.each { |channel| channel.read = channel.has_posts?(current_user, channel.last_post) }
       nil
     }, [:recent_channels]
     fetches :highlight_query, proc {
-      recent_channels.each do |channel|
-        channel.query = query
-      end
+      recent_channels.each { |channel| channel.query = query }
       nil
-    }
+    }, [:recent_channels]
 
-    fetches :count, proc { recent_channels.count }
+    fetches :count, proc { recent_channels.count }, [:recent_channels]
     fetches :start_index, proc { (page - 1) * per_page }
     fetches :end_index, proc { (page - 1) * per_page + recent_channels.size }, [:recent_channels]
     fetches :last_update, proc { (recent_channels.map(&:created_at) + recent_channels.map(&:updated_at) + recent_channels.map(&:last_post_date)).map(&:utc).max.to_i }, [:recent_channels]
